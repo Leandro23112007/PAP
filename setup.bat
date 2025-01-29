3ef2c9566ed01e81e64318c086bff9cde358ee15
Leandro23112007 <146172637+Leandro23112007@users.noreply.github.com>
2025-01-29 00:07:04 +0000

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -19,7 +19,7 @@ echo %CaminhoProdutos%
 echo CREATE DATABASE IF NOT EXISTS loja; > create_database.sql
 
 
-echo USE a; > create_tables.sql
+echo USE loja; > create_tables.sql
 echo CREATE TABLE IF NOT EXISTS historico_precos_venda (ID INT AUTO_INCREMENT PRIMARY KEY, codigo_produto INT, nome_produto VARCHAR(255), preco_venda DECIMAL(10,2), data_venda DATE, preco_compra DECIMAL(10,2), data_compra DATE); >> create_tables.sql
 echo CREATE TABLE IF NOT EXISTS inventario (ID INT AUTO_INCREMENT PRIMARY KEY, nome VARCHAR(255), preco_venda VARCHAR(50), codigo INT, quantidade INT, preco_compra DECIMAL(10,2), ultima_adicao DATETIME, vendas INT, data_venda VARCHAR(50)); >> create_tables.sql
 echo CREATE TABLE IF NOT EXISTS produtos (ID INT(11) AUTO_INCREMENT PRIMARY KEY, product_name VARCHAR(255), product_preco VARCHAR(50), product_estado VARCHAR(50), data_adicao TIMESTAMP, url VARCHAR(2048), nome_produto VARCHAR(255)); >> create_tables.sql
